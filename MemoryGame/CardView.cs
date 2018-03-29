b70f26cc7294843896ac5946d356af2f431c7f04
Abigail Cul <g00334291@gmit.ie>
2018-03-29 11:52:31 +0100

    Add project files.

---
+++ MemoryGame/CardView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame
+{
+    class CardView
+    {
+        public string StartGameButton { get; internal set; }
+
+        internal void FillUpGridViews()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MemoryGame/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Media.Animation;
+using System.Collections.ObjectModel;
+using Windows.Storage;
+using System.Text;
+using Windows.UI.ViewManagement;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MemoryGame
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+
+            this.InitializeComponent();
+            CardCollection = new CardView();
+            Loaded += MainPage_Loaded;
+
+            CardCollection.StartGameButton = "Start game";
+
+        }
+
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            CardCollection.FillUpGridViews();
+
+
+
+        }
+
+        internal CardView CardCollection { get; }
+    }
+
+
+
+    }
