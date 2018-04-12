a34d81e1006b139eb630297cec07b9e213c7d522
Abigail Cul <g00334291@gmit.ie>
2018-04-12 15:24:50 +0100

    Card class

---
+++ MemoryGame/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace MemoryGame
+{
+    class Card
+    {
+
+        private int moves = 0;
+        private int firstId = 0;
+        private int secondId = 0;
+        private Canvas first;
+        private Canvas second;
+        private int[,] board = new int[4, 4];
+        private List<int> matches = new List<int>();
+        private Random random = new Random((int)DateTime.Now.Ticks);
+
+        public Brush Background { get; set; }
+
+        public void Show(string content, string title)
+        {
+            IAsyncOperation<IUICommand> command = new MessageDialog(content, title).ShowAsync();
+        }
+
+       
+    }
+}
